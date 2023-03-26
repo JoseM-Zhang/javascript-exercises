19b16acbe936b245f7f4be2c65c4e41a1592d7a5
Zhang <126712665+JoseM-Zhang@users.noreply.github.com>
2023-03-25 21:27:25 -0500

    Solve 03

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,6 +1,9 @@
-const reverseString = function() {
-
-};
+const reverseString = function(inputString) {
+    let tempArray = Array.from(inputString);
+    tempArray = tempArray.reverse();
+    tempArray = tempArray.join('');
+    return tempArray;
+}
 
 // Do not edit below this line
 module.exports = reverseString;
