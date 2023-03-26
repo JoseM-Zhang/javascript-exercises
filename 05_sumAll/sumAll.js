d08ff46fcd431f5959d9d93155419d6db3fae54d
Zhang <126712665+JoseM-Zhang@users.noreply.github.com>
2023-03-25 21:53:51 -0500

    Solve 05

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,20 @@
-const sumAll = function() {
-
+const sumAll = function(firstNum, secondNum) {
+    let counter = 0;
+    if(typeof firstNum !== 'number' || typeof secondNum !== 'number')
+        return 'ERROR';
+    if(firstNum < 0 || secondNum < 0)
+        return 'ERROR';
+    if(firstNum > secondNum){
+        for(secondNum; secondNum<=firstNum; secondNum++){
+            counter += secondNum;
+        }
+    }
+    else {
+        for(firstNum; firstNum<=secondNum; firstNum++){
+            counter += firstNum;
+        }
+    }
+    return counter;
 };
 
 // Do not edit below this line
