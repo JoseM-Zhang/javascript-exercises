756623cb528aae9f490b5fbd2a56d95b9e8411b8
Zhang <126712665+JoseM-Zhang@users.noreply.github.com>
2023-03-25 20:52:35 -0500

    Solve 02

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,11 @@
-const repeatString = function() {
-
+const repeatString = function(inputString, times) {
+    let outputString = '';
+    if(times<0)
+        return 'ERROR';
+    for (let i =0; i<times; i++){
+        outputString += inputString;
+    }
+    return outputString;
 };
 
 // Do not edit below this line
