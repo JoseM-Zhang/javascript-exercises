57989a68e7fc10e11da213e2bcc0ca4def0c1b4c
Zhang <126712665+JoseM-Zhang@users.noreply.github.com>
2023-03-31 21:17:36 -0500

    solve 08

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,43 @@
-const add = function() {
+const add = function(a, b) {
+	return a + b;
+};
+
+const subtract = function(a, b) {
+	return a - b;
+};
+
+const sum = function(numbers) {
+	let sum = 0;
+  for(let num of numbers){
+    sum += num;
+  }
+  return sum;
+};
+
+const multiply = function(numbers) {
+  let sum = 1;
+  for(let n of numbers){
+    sum *= n;
+  }
+  return sum;
+
+};
+
+const power = function(a, n) {
+  let sum = 1;
+  for(let i = 0; i<n; i++){
+    sum *= a;
+  }
+  return sum;
 	
 };
 
-const subtract = function() {
-	
-};
-
-const sum = function() {
-	
-};
-
-const multiply = function() {
-
-};
-
-const power = function() {
-	
-};
-
-const factorial = function() {
-	
+const factorial = function(number) {
+	let sum = 1;
+  for(let i = 1; i<=number; i++){
+    sum *= i;
+  }
+  return sum;
 };
 
 // Do not edit below this line
