669ada608d5ce4940ec31beadd94c6aba259a3bd
Zhang <126712665+JoseM-Zhang@users.noreply.github.com>
2023-04-02 21:52:41 -0500

    Solve 12

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,16 @@
-const findTheOldest = function() {
-
+const findTheOldest = function(people) {
+    let oldest = people.sort((a, b) => {
+        if(!a.yearOfDeath)
+            a.yearOfDeath = new Date().getFullYear();
+        if(!b.yearOfDeath)
+            b.yearOfDeath = new Date().getFullYear();
+        
+        const lastPerson = a.yearOfDeath - a.yearOfBirth;
+        const nextPerson = b.yearOfDeath - b.yearOfBirth;
+        return lastPerson > nextPerson ? -1 : 1;
+    })[0];
+    console.log(oldest);
+    return oldest
 };
 
 // Do not edit below this line
